dfec21d5f23326dc3b19b763b9780edd07f7512a
Бажен Левковець <bazhen.paseka@gmail.com>
2021-02-24 20:18:10 +0200

    added #define MIN and MAX Temp

--- Drivers/Local_config/Thermostat_config.h
+++ Drivers/Local_config/Thermostat_config.h
@@ -3,8 +3,11 @@
 #define THERMOSTAT_CONFIG_H_INCLUDED
 //******************************************************************************************
 
-	#define SOFT_VERSION			140
+	#define SOFT_VERSION			150
 	#define ADR_I2C_FC113 			0x27
+
+	#define	TEMP_MAX				1700
+	#define	TEMP_MIN				1600
 
 //******************************************************************************************
 #endif	//	THERMOSTAT_CONFIG_H_INCLUDED
